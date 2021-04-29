25ccaeee26e6a5c9f73782382baca58bfae929e5
Vladimir Chernov <v.chernov@zebrains.team>
2021-04-29 16:41:22 +0300

    Создали проект. Добавили компонент Link в компонент Header, который позваляет не перезагружать старинцу при переходе по ссылкам находящимся в шапке сайта

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return <footer className="page-footer green lighten-1">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright Text
+      <a
+        className="grey-text text-lighten-4 right"
+        href="https://github.com/VladimirChernovv/Project_shop"
+        target="_blank"
+        rel="noreferror"
+      >
+        Repo
+      </a>
+      </div>
+    </div>
+  </footer>
+};
+
+export {Footer};
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// Компонент позваляющий при клике на ссылку не перезагружать js код
+import {Link} from 'react-router-dom';
+
+function Header() {
+  return  <nav className="green lighten-3">
+    <div className="nav-wrapper">
+      <Link to="/" className="brand-logo">React Shop</Link>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contacts">Conacts</Link></li>
+      </ul>
+    </div>
+  </nav>
+};
+
+export {Header};
